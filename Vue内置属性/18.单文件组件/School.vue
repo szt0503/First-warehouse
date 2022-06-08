e51951ba5a01394b116622eae7777259a2ea9b6c
szt0503 <107089374+szt0503@users.noreply.github.com>
2022-06-08 10:19:36 +0800

    Add files via upload

---
+++ Vue内置属性/18.单文件组件/School.vue
@@ -0,0 +1,33 @@
+
+<template>
+<!-- 组件的结构 -->
+<div>
+    <h2>学校名称:{{name}}</h2>
+    <h2>学校地址:{{address}}</h2>
+    <button @click = "showNum">点我看VM里的X值</button>
+    </div>
+</template>
+
+<script>
+// 组件交互相关的代码
+export default {
+            data() {
+                return {
+                    name:'百科园',
+                    address:'地球'
+                }
+            },
+            methods: {
+                showNum(){
+                    console.log(this.x);
+                }
+            },
+        }
+</script>
+
+<style>
+/* 样式标签 */
+.dmeo{
+    background-color: orange;
+}
+</style>
---
+++ Vue内置属性/18.单文件组件/Student.vue
@@ -0,0 +1,33 @@
+
+<template>
+<!-- 组件的结构 -->
+<div>
+    <h2>学生姓名:{{name}}</h2>
+    <h2>学生年龄:{{age}}</h2>
+    <button @click = "showNum">点我看VM里的X值</button>
+    </div>
+</template>
+
+<script>
+// 组件交互相关的代码
+export default {
+            data() {
+                return {
+                    name:'小舞',
+                    age:'18'
+                }
+            },
+            methods: {
+                showNum(){
+                    console.log(this.x);
+                }
+            },
+        }
+</script>
+
+<style>
+/* 样式标签 */
+.dmeo{
+    background-color: orange;
+}
+</style>
